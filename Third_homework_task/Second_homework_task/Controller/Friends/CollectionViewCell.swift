14d30763511ffd2b9816dfad579d5b0ddb1ff76a
Nihad <the_nihad@icloud.com>
2020-11-02 10:14:41 +0300

    Solution to the third homework task

---
+++ Third_homework_task/Second_homework_task/Controller/Friends/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Second_homework_task
+//
+//  Created by Nihad on 11/2/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
